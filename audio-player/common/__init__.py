dcf9883c44c09d275c8ed18919fd8e6ec6ee55a3
Michel Kohanim <michel@universal-devices.com>
2024-01-10 10:25:22 -0800

    initial release

--- audio-player/common/__init__.py
+++
@@ -1,8 +0,0 @@
-#!/usr/bin/env python3
-"""
-Copyright (C) 2023 Universal Devices 
-
-"""
-
-#Module init file. Nothing to do here for now
-
--- audio-player/nls_gen.py
+++ audio-player/nls_gen.py
@@ -2,10 +2,47 @@ import os
 import udi_interface
 from typing import List
 import re
-from default_nls import DefaultNls
 LOGGER = udi_interface.LOGGER
 
+
+class DefaultNls() :
+
+    nls = '''
+#Controller
+ND-mpgPlayer-NAME = MPG Player 
+ND-mpgPlayer-ICON = Siren
+
+ST-mpgPlayer-ST-NAME = Playing Status 
+ST-mpgPlayer-GV0-NAME = Play List 
+
+#Shared command names
+
+CMD-STOP-NAME = Stop 
+CMD-PLAY-NAME = Play 
+CMDP-mpgNames-PLAYLIST-NAME = Playlist
+
+PLAYSTATUS-0 = Idle
+PLAYSTATUS-1 = Playing
+
+MPGNAME-0 = None 
+    
+'''
+
+class DefaultEditor ():
+    editors='''
+    <editors>
+    	<editor id="playStatus">
+	    	<range uom="25" min="0" max="1" prec="0" nls="PLAYSTATUS" /> 
+    	</editor>
+        <editor id="mpgNames">
+            <range uom="25" min="0" max="100" nls="MPGNAME"/>
+        </editor>
+    </editors>
+    '''
+
 class NLSGenerator :
+
+    musicList:[str] = []
         
     def __init__(self) -> None:
         pass
@@ -36,19 +73,36 @@ class NLSGenerator :
             self.make_file_dir(en_us_txt)
             nls = open(en_us_txt,  "w")
             nls.write(DefaultNls.nls)
-            index: int = 0
+            self.musicList.append("None")
+            index: int = 1
             with os.scandir(path) as entries:
                 for entry in entries:
                     name = self.isMusicFile(entry)
                     if name != None:
-                        nls.write("MPGNAME-{} = {}".format(index, name))
+                        nls.write("MPGNAME-{} = {}\n".format(index, name))
+                        self.musicList.append(name)
                         index+=1
             nls.write("\n\n")
-
             nls.close()
+
+## in case we want to update the editors with actual max number of songs
+#            editor_xml = "profile/editor/editor.xml"
+#            editor = open(editor_xml, "w")
+#            editors = DefaultEditor.editors.replace("max=\"100\"", "max=\"{}\"".format(index))
+#            editor.write(editors)
+#            editor.close()
         except Exception as e:
             LOGGER.error('Failed to generate NLS: {}'.format(str(e)))
 
+    def getFilePath(self, parent_path:str, index: int):
+        if parent_path == None:
+            LOGGER.error('Need parent path')
+            return None
+        if index < 0 or index >= len(self.musicList):
+            LOGGER.error('Failed getting path requested index {}, array len {}'.format(index, len(self.musicList)))
+            return None
+
+        return parent_path + "/" + self.musicList[index] 
 
     def make_file_dir(self, file_path):
         directory = os.path.dirname(file_path)
@@ -57,10 +111,10 @@ class NLSGenerator :
         return True
 
 
-if __name__ == "__main.test__":
+if __name__ == "__main__":
     try:
         nlsGen = NLSGenerator()
-        nlsGen.generate("/usr/home/admin/workspace/plugin-dev/audio-player")
+        nlsGen.generate("/usr/home/admin/Chimes")
 
     except (KeyboardInterrupt, SystemExit):
         sys.exit(0)
